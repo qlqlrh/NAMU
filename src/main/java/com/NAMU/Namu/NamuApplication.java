4a08f4b996e2e680b27e8d50cb30d23bd51ee3dd
SSH <laporian@naver.com>
2024-11-11 10:25:05 +0900

    스프링 프로젝트

---
+++ src/main/java/com/NAMU/Namu/NamuApplication.java
@@ -0,0 +1,13 @@
+package com.NAMU.Namu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NAMU/Namu/NamuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NAMU.Namu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
