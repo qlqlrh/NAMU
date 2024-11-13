153aee709a9f03ac3e9d3e788d67f8e499c9c67e
qlqlrh <dongin@pusan.ac.kr>
2024-11-13 15:26:45 +0900

    Menu 엔티티 구성

--- src/main/java/com/green/namu/NamuApplication.java
+++ src/main/java/com/green/namu/NamuApplication.java
@@ -2,12 +2,8 @@ package com.green.namu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing // 자동 감사 기능(Auditing) 활성화
-@ConfigurationPropertiesScan
 public class NamuApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/green/namu/domain/Menu.java
+++ src/main/java/com/green/namu/domain/Menu.java
@@ -1,21 +1,16 @@
 package com.green.namu.domain;
 
-import com.green.namu.common.entity.BaseEntity;
-import com.green.namu.domain.status.MenuStatus;
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@EntityListeners(AuditingEntityListener.class) // Auditing 기능을 활성화하는 리스너
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 파라미터가 없는 기본 생성자를 자동으로 생성
-@AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자를 자동 생성
-@Builder
-public class Menu extends BaseEntity {
+//@AllArgsConstructor // 모든 필드를 매개변수로 받는 생성자를 자동 생성
+public class Menu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
@@ -36,25 +31,16 @@ public class Menu extends BaseEntity {
     @Column(name = "menu_picture_url")
     private String menuPictureUrl;
 
-    @Column(name = "popularity") // columnDefinition = "TINYINT(1) DEFAULT 0 <- h2가 인식 못함
+    @Column(name = "popularity", columnDefinition = "TINYINT(1) DEFAULT 0")
     private Boolean popularity = false;
 
     @Column(name = "menu_detail")
     private String menuDetail;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "menu_category", nullable = false)
-    private Category menuCategory;
+    private String menuCategory;
 
     @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING) // enum의 값을 문자열 형태로 데이터베이스에 저장 (인덱스 형태가 아니라)
     private MenuStatus status = MenuStatus.ON_SALE;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "store_id", nullable = false)
-    private Store store;
-
-    public void setStore(Store store) {
-        this.store = store;
-    }
 }
