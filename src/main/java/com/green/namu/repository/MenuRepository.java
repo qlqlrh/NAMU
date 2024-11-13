013220274815fccf7f814094e3bbb15a386f5a3e
qlqlrh <dongin@pusan.ac.kr>
2024-11-13 16:10:39 +0900

    Menu DTO

---
+++ src/main/java/com/green/namu/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.green.namu.repository;
+
+import com.green.namu.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
