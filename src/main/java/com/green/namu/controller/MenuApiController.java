166ffd90d19cf72713d79e0820e93deb3b87590b
qlqlrh <dongin@pusan.ac.kr>
2024-11-15 03:03:10 +0900

    특정 Menu 조회 API

--- src/main/java/com/green/namu/controller/MenuApiController.java
+++ src/main/java/com/green/namu/controller/MenuApiController.java
@@ -2,15 +2,14 @@ package com.green.namu.controller;
 
 import com.green.namu.domain.Menu;
 import com.green.namu.dto.AddMenuRequest;
+import com.green.namu.dto.MenuReadResponse;
 import com.green.namu.dto.MenuSaveResponse;
 import com.green.namu.service.MenuService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController // HTTP ResponseBody에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -25,5 +24,13 @@ public class MenuApiController {
         // 요청한 자원이 성공적으로 생성되었음과 응답 객체를 전송
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @GetMapping("/menu/{menuId}")
+    public ResponseEntity<MenuReadResponse> getMenuById(@PathVariable("menuId") Long menuId) {
+        MenuReadResponse response = menuService.findById(menuId);
+
+        return ResponseEntity.ok()
+                .body(response); // 200 OK 응답
     }
 }
