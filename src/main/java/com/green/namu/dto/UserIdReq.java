272705b400f0a5f08e3eecd2e22eadaa6dc538c6
qlqlrh <dongin@pusan.ac.kr>
2025-01-18 04:06:18 +0900

    토큰 발급 API 추가, User에 refresh token 필드 추가

--- src/main/java/com/green/namu/dto/UserIdReq.java
+++ src/main/java/com/green/namu/dto/UserIdReq.java
@@ -1,14 +1,2 @@
-package com.green.namu.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserIdReq {
-    private Long userId;
+package com.green.namu.dto;public class UserIdReq {
 }
